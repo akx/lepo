cf29df97914083a34a5b44cbba715bd095443716
Aarni Koskela <akx@iki.fi>
2017-05-15 21:09:09 +0300

    Add cascadeable consumes/produces to Operations
    Refs #6

--- lepo/operation.py
+++ lepo/operation.py
@@ -1,3 +1,6 @@
+from django.utils.functional import cached_property
+
+
 class Operation:
     def __init__(self, api, path, data):
         """
@@ -17,3 +20,27 @@ class Operation:
     def parameters(self):
         return self.data.get('parameters', ())
 
+    def _get_overridable(self, key, default=None):
+        # TODO: This probes a little too deeply into the specifics of these objects, I think...
+        for obj in (
+            self.data,
+            self.path.mapping,
+            self.api.api,
+        ):
+            if key in obj:
+                return obj[key]
+        return default
+
+    @cached_property
+    def consumes(self):
+        value = self._get_overridable('consumes', [])
+        if not isinstance(value, (list, tuple)):
+            raise TypeError('`consumes` must be a list, got %r' % value)
+        return value
+
+    @cached_property
+    def produces(self):
+        value = self._get_overridable('produces', [])
+        if not isinstance(value, (list, tuple)):
+            raise TypeError('`produces` must be a list, got %r' % value)
+        return value
