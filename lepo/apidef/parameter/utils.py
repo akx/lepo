05c7611ff35091c4dd5279a457566fb075d85aec
Aarni Koskela <akx@iki.fi>
2018-07-10 21:31:06 +0300

    Move body decoders to a file of their own

--- lepo/apidef/parameter/utils.py
+++ lepo/apidef/parameter/utils.py
@@ -1,10 +1,9 @@
-import json
-
 import jsonschema
 from django.core.files import File
 from django.utils.encoding import force_text
 from jsonschema import Draft4Validator
 
+from lepo.decoders import get_decoder
 from lepo.excs import InvalidBodyContent
 from lepo.utils import maybe_resolve
 
@@ -34,16 +33,15 @@ def read_body(request, parameter=None):
         if parameter.type == 'binary':
             return request.body.read()
     try:
-        if request.content_type == 'application/json':
-            return json.loads(request.body.decode(request.content_params.get('charset', 'UTF-8')))
-        elif request.content_type == 'text/plain':
-            return request.body.decode(request.content_params.get('charset', 'UTF-8'))
         if request.content_type == 'multipart/form-data':
             # TODO: this definitely doesn't handle multiple values for the same key correctly
             data = dict()
             data.update(request.POST.items())
             data.update(request.FILES.items())
             return data
+        decoder = get_decoder(request.content_type)
+        if decoder:
+            return decoder(request.body, encoding=request.content_params.get('charset', 'UTF-8'))
     except Exception as exc:
         raise InvalidBodyContent('Unable to parse this body as %s' % request.content_type) from exc
     raise NotImplementedError('No idea how to parse content-type %s' % request.content_type)  # pragma: no cover
---
+++ lepo/decoders.py
@@ -0,0 +1,36 @@
+import json
+
+from django.utils.encoding import force_text
+
+DEFAULT_ENCODING = 'utf-8'
+
+
+def decode_json(content, encoding=None, **kwargs):
+    return json.loads(force_text(content, encoding=(encoding or DEFAULT_ENCODING)))
+
+
+def decode_plain_text(content, encoding=None, **kwargs):
+    return force_text(content, encoding=(encoding or DEFAULT_ENCODING))
+
+
+DECODERS = {
+    'application/json': decode_json,
+    'text/plain': decode_plain_text,
+}
+
+
+def get_decoder(content_type):
+    """
+    Get a decoder function for the content type given, or None if there is none.
+
+    :param content_type: Content type string
+    :type content_type: str
+    :return: function or None
+    """
+    if content_type.endswith('+json'):  # Process all +json vendor types like JSON
+        content_type = 'application/json'
+
+    if content_type in DECODERS:
+        return DECODERS[content_type]
+
+    return None
